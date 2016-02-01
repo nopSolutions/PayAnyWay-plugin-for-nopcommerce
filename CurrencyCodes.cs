907b2f4e4c97e713535d3aa9c58e680f817235b1
skoshelev <sergey.k@nopcommerce.com>
2016-02-01 17:42:08 +0300

    Added supported ISO currency code enum

---
+++ CurrencyCodes.cs
@@ -0,0 +1,12 @@
+﻿namespace Nop.Plugin.Payments.MonetaDirect
+{
+    /// <summary>
+    /// Supported ISO currency code
+    /// </summary>
+    public enum CurrencyCodes
+    {
+        RUB,
+        USD,
+        EUR
+    }
+}
