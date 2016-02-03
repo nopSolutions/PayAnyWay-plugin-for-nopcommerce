17e7796ccd1d60dbfe158adc58ca01b2e9f49190
skoshelev <sergey.k@nopcommerce.com>
2016-02-03 16:34:49 +0300

    Added route provider

---
+++ RouteProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Payments.MonetaDirect
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            //ConfirmPay
+            routes.MapRoute("Plugin.Payments.MonetaDirect.ConfirmPay",
+                 "Plugins/MonetaDirect/ConfirmPay",
+                 new { controller = "PaymentMonetaDirect", action = "ConfirmPay" },
+                 new[] { "Nop.Plugin.Payments.MonetaDirect.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
